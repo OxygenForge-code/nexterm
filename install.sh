52bf233b6321c3d8b038f84858b9917ada6fd270
OxygenForge-code <oxygenforge@gmail.com>
2026-02-19 21:55:59 +0300

    Add installation script for NexTerm

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "Installing NexTerm..."
+
+chmod +x nexterm
+cp nexterm $PREFIX/bin/
+
+echo ""
+echo "Installation complete!"
+echo "You can now run it by typing: nexterm"
